0d7bea7175bf3326604c554bb7f3b97fb2bf90e7
Arvish Doshi <doshi.arvish16@gmail.com>
2025-06-27 00:04:21 +0530

    Handle AI requests.

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -1,6 +1,12 @@
 from celery import shared_task
 
+from core import models
 
 @shared_task
 def hello_task(name):
     print(f"Hello {name}. You have {len(name)} characters in your name.")
+
+
+@shared_task
+def handle_openai_request_job(openai_request_id):
+    models.OpenAiRequest.objects.get(id=openai_request_id).handle()
